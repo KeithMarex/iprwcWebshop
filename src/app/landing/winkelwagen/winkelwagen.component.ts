c64a2ddb66506cdffd950165bdda3ac87ff54871
KoennieTime <koenvandermarel@gmail.com>
2020-12-28 09:30:34 +0100

    Vergeten te pushen Account + Winkelwagen tab

---
+++ src/app/landing/winkelwagen/winkelwagen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-winkelwagen',
+  templateUrl: './winkelwagen.component.html',
+  styleUrls: ['./winkelwagen.component.scss']
+})
+export class WinkelwagenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
